e20eae59cac94e73f8ca1e63ab9cc430fc9cc25d
Mosadek <48337574+AhmedMosadek@users.noreply.github.com>
2024-05-22 16:11:16 +0300

    GroupAnagrams.java

---
+++ src/GroupAnagrams.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    private static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : strs) {
+            char[] a = s.toCharArray();
+            Arrays.sort(a);
+            String key = String.valueOf(a);
+            if(!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+}
